6abd9d81eae6ea90c14bdb21f00802b5f49f9bc4
EkchendaSonarith <thenarytb@gmail.com>
2026-09-06 19:08:52 +0700

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "images.shadcnspace.com" },
+      { protocol: "https", hostname: "sombobaeb.cheat.casa" },
+    ],
+  },
 };
 
 export default nextConfig;
